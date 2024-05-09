521adc3ae7602557a6c7598e72d583ae8fa8765f
celiiaaa <celia.patricio.ferrer@gmail.com>
2024-05-09 16:42:53 +0200

    fin??

--- p3/store_manager.c
+++ p3/store_manager.c
@@ -16,9 +16,13 @@ int COSTE[5] = {2, 5, 15, 25, 100};
 int PRECIO[5] = {3, 10, 20, 40, 125};
 
 pthread_mutex_t mutex_prod;
+pthread_mutex_t mutex_cons;
 pthread_cond_t cond_prod;
+pthread_cond_t cond_cons;
+
 
 int next_op_prod = 0;
+int next_op_cons = 0;
 
 /* Argumentos del hilo productor */
 typedef struct arg_productor {
@@ -59,10 +63,10 @@ void *productor(void *arg) {
             return NULL;
         }
         *operacion = lista_operaciones[ids[i]];
-        printf("Operacion %d\n", ids[i]);
+        /* printf("Operacion %d\n", ids[i]);
         printf("\tProducto: %d\n", operacion->product_id);
         printf("\tOperacion: %d\n", operacion->op);
-        printf("\tUnidades: %d\n", operacion->units);
+        printf("\tUnidades: %d\n", operacion->units); */
 
         // Insertar la operación en la cola
         queue_put(cola_circular, operacion);
@@ -87,13 +91,75 @@ void *productor(void *arg) {
 typedef struct arg_consumidor {
     struct queue *buffer;
     int max_op;
+    int *ids;
     int *profit;
     int *product_stock;
 } arg_consumidor;
 
 /* Función del hilo consumidor */
 void *consumidor(void *arg) {
+    pthread_mutex_lock(&mutex_cons);
     printf("Consumidor\n");
+
+    // Obtener los argumentos
+    arg_consumidor *args = (arg_consumidor *) arg;
+    queue *cola_circular = args->buffer;
+    int max_op = args->max_op;
+    int *ids = args->ids;
+    int *profit = args->profit;
+    int *product_stock = args->product_stock;
+
+    // Imprimir
+    /* printf("IDS: ");
+    for (int i = 0; i < max_op; i++) {
+        printf("%d ", ids[i]);
+    }
+    printf("\n"); */
+
+    // Realizar las operaciones
+    for (int i = 0; i < max_op; i++) {
+        
+        while (ids[i] != next_op_cons) {
+            pthread_cond_wait(&cond_prod, &mutex_cons);
+        }
+
+        // Obtener la operación de la cola
+        struct element *operacion = queue_get(cola_circular);
+        if (operacion == NULL) {
+            perror("Error. No se pudo obtener la operación de la cola.\n");
+            return NULL;
+        }
+
+        /* printf("Operacion %d\n", ids[i]);
+        printf("\tProducto: %d\n", operacion->product_id);
+        printf("\tOperacion: %d\n", operacion->op);
+        printf("\tUnidades: %d\n", operacion->units); */
+
+        // Realizar la operación
+        if (operacion->op == 0) {
+            // Compra
+            *profit -= COSTE[operacion->product_id-1] * operacion->units;
+            product_stock[operacion->product_id-1] += operacion->units;
+        } else {
+            // Venta
+            *profit += PRECIO[operacion->product_id-1] * operacion->units;
+            product_stock[operacion->product_id-1] -= operacion->units;
+        }
+
+        printf("Cola: ");
+        print_queue(cola_circular);
+
+        next_op_cons++;
+        pthread_cond_broadcast(&cond_cons);
+    }
+
+
+    pthread_mutex_unlock(&mutex_cons);
+
+    // Liberar memoria
+    free(ids);
+    free(args);
+
     return NULL;
 }
 
@@ -103,6 +169,7 @@ int main (int argc, const char *argv[]) {
     int product_stock[5] = {0, 0, 0, 0, 0};
 
     pthread_mutex_init(&mutex_prod, NULL);
+    pthread_mutex_init(&mutex_cons, NULL);
     pthread_cond_init(&cond_prod, NULL);
 
     // Comprobar el número argumentos
@@ -192,15 +259,17 @@ int main (int argc, const char *argv[]) {
         return -1;
     }
 
+    int op_actual;
+
     // Productores y consumidores
     pthread_t productores[n_prod];
-    // pthread_t consumidores[n_cons];
+    pthread_t consumidores[n_cons];
 
     // Reparto equitativo de las operaciones entre los productores
     int n_op_prod = max_op_file / n_prod;
     int op_rest_prod = max_op_file % n_prod;
     
-    int op_actual = 0;
+    op_actual = 0;
     for (int i = 0; i < n_prod; i++) {
         int n_op_actual = n_op_prod;
         if (op_rest_prod > 0) {
@@ -237,6 +306,48 @@ int main (int argc, const char *argv[]) {
         }
     }
 
+    // Reparto equitativo de las operaciones entre los consumidores
+    int n_op_cons = max_op_file / n_cons;
+    int op_rest_cons = max_op_file % n_cons;
+
+    op_actual = 0;
+    for (int i = 0; i < n_cons; i++) {
+        int n_op_actual = n_op_cons;
+        if (op_rest_cons > 0) {
+            n_op_actual++;
+            op_rest_cons--;
+        }
+
+        printf("Consumidor %d realizará %d operacion(es)\n", i + 1, n_op_actual);
+
+        // Argumentos para el hilo consumidor
+        arg_consumidor *arg = malloc(sizeof(arg_consumidor));
+        if (arg == NULL) {
+            perror("Error. No se pudo reservar memoria para los argumentos.\n");
+            return -1;
+        }
+        int *ids = malloc(n_op_actual * sizeof(int));
+        if (ids == NULL) {
+            perror("Error. No se pudo reservar memoria para los ids.\n");
+            return -1;
+        }
+        for (int j = 0; j < n_op_actual; j++) {
+            ids[j] = op_actual;
+            op_actual++;
+        }
+        arg->ids = ids;
+        arg->buffer = cola_circular;
+        arg->max_op = n_op_actual;
+        arg->profit = &profit;
+        arg->product_stock = product_stock;
+
+        // Crear hilo consumidor
+        if (pthread_create(&consumidores[i], NULL, &consumidor, arg) != 0) {
+            perror("Error. No se pudo crear el hilo consumidor.\n");
+            return -1;
+        }
+    }
+
     // Esperar a que terminen los hilos productores
     for (int i = 0; i < n_prod; i++) {
         if (pthread_join(productores[i], NULL) != 0) {
@@ -245,6 +356,13 @@ int main (int argc, const char *argv[]) {
         }
     }
 
+    // Esperar a que terminen los hilos consumidores
+    for (int i = 0; i < n_cons; i++) {
+        if (pthread_join(consumidores[i], NULL) != 0) {
+            perror("Error. No se pudo esperar al hilo consumidor.\n");
+            return -1;
+        }
+    }
 
     // Output
     printf("Total: %d euros\n", profit);
@@ -259,6 +377,7 @@ int main (int argc, const char *argv[]) {
     /* queue_destroy(cola_circular); */
 
     pthread_mutex_destroy(&mutex_prod);
+    pthread_mutex_destroy(&mutex_cons);
     pthread_cond_destroy(&cond_prod);
 
     return 0;
